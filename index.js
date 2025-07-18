516847e87b45e94d276d6bcbde587355bf0bf2b4
Ibrahim Zaheer <ibrahimzah33r@gmail.com>
2025-07-18 02:04:29 +0100

    Done

--- index.js
+++ index.js
@@ -21,12 +21,45 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({greeting: 'hello bob'});
 });
 
+function isValidDate(date) {
+  return !isNaN(new Date(date).getTime());
+}
 
+app.get('/api/:date?', (req, res) => {
+  const { date } = req.params;
 
-// Listen on port set in environment variable or default to 3000
+  let response = {};
+
+  if (!date) {
+    // No date provided, return current time
+    const currentDate = new Date();
+    response.unix = currentDate.getTime();
+    response.utc = currentDate.toUTCString();
+  } else {
+    // If the date is a number = Unix
+    let parsedDate;
+    if (!isNaN(date)) {
+      parsedDate = new Date(parseInt(date));
+    } else {
+      // Otherwise, try UTC
+      parsedDate = new Date(date);
+    }
+
+    if (isValidDate(parsedDate)) {
+      response.unix = parsedDate.getTime();
+      response.utc = parsedDate.toUTCString();
+    } else {
+      response.error = "Invalid Date";
+    }
+  }
+
+  res.json(response);
+});
+
+// Listen on port set in environment variable o@r default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
